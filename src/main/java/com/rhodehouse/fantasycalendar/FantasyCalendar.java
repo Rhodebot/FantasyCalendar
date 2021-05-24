6c92a56571b3ec6a0b33b35bd1880aec92740a31
Rhode <Rhode@FrootLoops>
2021-05-24 15:03:10 -0700

    testing

--- src/main/java/com/rhodehouse/fantasycalendar/FantasyCalendar.java
+++ src/main/java/com/rhodehouse/fantasycalendar/FantasyCalendar.java
@@ -1,14 +1,13 @@
 package com.rhodehouse.fantasycalendar;
 
-import java.awt.GridLayout;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import javax.print.PrintService;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JSpinner;
@@ -20,6 +19,21 @@ import static j2html.TagCreator.*;
 public class FantasyCalendar{
 
 	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JButton genButton = new JButton("Generate");
+		genButton.addActionListener(.new GenerateButtonHandler());
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(new JSpinner(), BorderLayout.NORTH);
+		frame.add(new MonthView(), BorderLayout.CENTER);
+		frame.add(genButton, BorderLayout.SOUTH);
+		
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+	
+	private static void generate() {
 		File f = new File("test.html");
 		Month m = new Month (31, 7, 5);
 		System.out.print(m);
@@ -33,49 +47,12 @@ public class FantasyCalendar{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		JFrame frame = new JFrame();
-		JButton genButton = new JButton();
-		frame.setLayout(new GridLayout(3, 1));
-		
-		frame.add(new JSpinner());
-		frame.add(new MonthView());
-		frame.add(genButton);
-		
-		frame.pack();
-		frame.setVisible(true);
-		
-		
-		
-		/*
-		File f;
-		FileWriter writ;
-		ArrayList<ContainerTag> a = new ArrayList<ContainerTag>();
-		
-		a.add(table(tr(td(p("a1")),td(p("a2"))),tr(td(p("a3")),td(p("a4")))));
-		a.add(table(tr(td(p("b1")),td(p("b2"))),tr(td(p("b3")),td(p("b4")))));
-		
-		f = new File("test.html");
-		try {
-			f.createNewFile();
-			writ = new FileWriter(f);
-			ContainerTag fileBody = body(
-					p("testing..."), p("twasting'''"),
-					each(a, tbl -> tbl)
-				);
-			String fileContents = 
-			html(
-				head(
-					title("Test File")
-				), fileBody
-			).render();
-			writ.write(fileContents);
-			writ.close();
-		} catch (IOException e) {
-			return;
+	}
+	
+	private class GenerateButtonHandler implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			generate();
 		}
-		*/
-		
 	}
 
 }
